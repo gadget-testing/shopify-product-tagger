31cba36cb52a4170e20f33096b8561d2b8bc3400
Matin <matin.damyar@gmail.com>
2023-10-20 15:28:35 -0400

    Update schema.gadget.ts

--- models/shopifyProduct/schema.gadget.ts
+++ models/shopifyProduct/schema.gadget.ts
@@ -3,26 +3,27 @@ import type { GadgetModel } from "gadget-server";
 // This metadata describes a model's schema
 // View and edit your model in the Gadget editor at https://myapp.gadget.dev/edit
 export const schema: GadgetModel = {
-  type: "gadget/model-schema/v1",
-  shopifyModel: "product",
-  dbKey: g2451,
-  shopifyFields: {
-    body: { dbKey: "j4513" },
-    "body", 
-    "handle", 
-    "productCategory", 
-    "productType", 
-    "publishedAt", 
-    "publishedScope", 
-    "shop", 
-    "shopifyCreatedAt", 
-    "shopifyUpdatedAt", 
-    "status", 
-    "tags", 
-    "templateSuffix", 
-    "title", 
-    "vendor"
+  dbTableId: "sp-aaaa",
+  shopify: {
+    model: "product",
+    fields: {
+      "body", 
+      "handle", 
+      "productCategory", 
+      "productType", 
+      "publishedAt", 
+      "publishedScope", 
+      "shop", 
+      "shopifyCreatedAt", 
+      "shopifyUpdatedAt", 
+      "status", 
+      "tags", 
+      "templateSuffix", 
+      "title", 
+      "vendor"
+    }
   },
   fields: {
+    myVeryCoolDangler: { type: "String", validations: [{ type: "required" }], dbFieldId: "pl-vava", },
   },
 };
