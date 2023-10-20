7729e50028d1ba07c3c15fe92faa3d5b9183c975
Matin <matin.damyar@gmail.com>
2023-10-20 15:23:31 -0400

    Update schema.gadget.ts

--- models/plan/schema.gadget.ts
+++ models/plan/schema.gadget.ts
@@ -1,12 +1,12 @@
 import type { GadgetModel } from "gadget-server";
 
-// This metadata describes a model's schema
 // View and edit your model in the Gadget editor at https://myapp.gadget.dev/edit
+
 export const schema: GadgetModel = {
-  dbKey: "g4553",
+  dbTableId: "pl-aaaa",
   fields: {
-    name: { type: "String", validations: [{ type: "required" }], dbKey: "j4513" },
-    description: { type: "RichText", validations: [{ type: "required" }], dbKey: "g5263" },
-    shop: { type: "BelongsTo", relatedModel: "shopifyShop", dbKey: "h4563" },
+    name: { type: "String", validations: [{ type: "required" }], dbFieldId: "pl-vava", },
+    description: { type: "RichText", validations: [{ type: "required" }], dbFieldId: "pl-addd" },
+    shop: { type: "BelongsTo", relatedModel: "shopifyShop", dbFieldId: "pl-ewef" },
   },
 };
